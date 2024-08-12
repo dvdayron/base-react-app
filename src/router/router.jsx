25510eac527463ba5895355f793545021b5e80db
Dayron Ramida Coll <dvdayron@gmail.com>
2024-08-12 12:56:37 -0500

    add configs

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-vars */
-import { createBrowserRouter } from 'react-router-dom'
-
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { AuthRoutes } from '../auth'
 import { AppRoutes } from '../ui'
 
 export const router = createBrowserRouter([
     AuthRoutes,
     AppRoutes,
+    {
+        path: '/*',
+        element: <Navigate to='/'/>,
+    },
 ]);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+/* eslint-disable no-undef */
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), 'VITE_APP');
+  const envWithProcessPrefix = {
+    'process.env': `${JSON.stringify(env)}`,
+  };
+
+  return {
+    define: envWithProcessPrefix,
+    plugins: [react()],
+  }
 })
